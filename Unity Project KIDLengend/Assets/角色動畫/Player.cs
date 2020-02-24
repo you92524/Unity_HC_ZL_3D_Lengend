98a550681f59ba5d567dfcc81b59f6c9b674bbaa
you92524 <57396776+you92524@users.noreply.github.com>
2020-02-25 02:00:23 +0800

    test

---
+++ Unity Project KIDLengend/Assets/角色動畫/Player.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rig;
+    public float speed=25f;
+
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        print("按");
+    }
+    private void FixedUpdate()
+    {
+        Move();
+      
+
+    }
+    private void Move()
+    {
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 pos = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rig.AddForce(pos * speed);
+    }
+   
+}
