ae0cff9640b7abed8e7588e7716bc3e2fb5291fa
you92524 <57396776+you92524@users.noreply.github.com>
2020-02-25 18:14:36 +0800

    攻擊動作完成

--- Unity Project KIDLengend/Assets/角色動畫/Player.cs
+++ Unity Project KIDLengend/Assets/角色動畫/Player.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody rig;
+    private Animator ani;
+    private float timer; 
+    
+
     public float speed=25f;
+    public float cd = 0.9f;
 
 
     private void Start()
     {
         rig = GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
         print("按");
     }
     private void FixedUpdate()
     {
         Move();
-      
+        Attack();
 
     }
     private void Move()
@@ -28,6 +34,30 @@ public class Player : MonoBehaviour
         Vector3 pos = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rig.AddForce(pos * speed);
+        ani.SetBool("移動開關", moveVertical != 0 || moveHorizontal != 0);
     }
-   
+
+    private void Dead()
+    {
+        ani.SetBool("死亡開關", true);                   
+        enabled = false;                                
+       
+    }
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && timer > cd)            // 如果 計時器 < 冷卻時間
+        {
+            
+            ani.SetTrigger("攻擊觸發");
+            print("攻擊");
+            timer = 0;
+        }
+        else 
+        {
+           
+            timer += Time.deltaTime;
+        }
+        
+    }
+    
 }
